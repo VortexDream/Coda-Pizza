19ff8c1cb82eeb437ad76c7222d74956e21f8ccd
Иван Былёв <ivan.bylyov@gmail.com>
2023-11-03 18:38:53 +0300

    Chapters 26 + 27

---
+++ app/src/main/java/com/vortex/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -0,0 +1,106 @@
+package com.vortex.android.codapizza.ui
+
+import android.icu.text.NumberFormat
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import com.vortex.android.codapizza.R
+import com.vortex.android.codapizza.model.Pizza
+import com.vortex.android.codapizza.model.Topping
+import com.vortex.android.codapizza.model.ToppingPlacement
+
+@Preview
+@Composable
+fun PizzaBuilderScreen(
+    modifier: Modifier = Modifier
+) {
+    var pizza by rememberSaveable { mutableStateOf(Pizza()) }
+
+    Column(
+        modifier = modifier
+    ) {
+        ToppingList(
+            pizza = pizza,
+            onEditPizza = { pizza = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f, fill = true)
+        )
+
+        OrderButton(
+            pizza = pizza,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+private fun ToppingList(
+    pizza: Pizza,
+    //Лямбда, вызываемая при каждом редактирование переменной pizza
+    onEditPizza: (Pizza) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(Topping.values()) {topping ->
+            ToppingCell(
+                topping = topping,
+                placement = pizza.toppings[topping],
+                onClickTopping = {
+                    val isOnPizza = pizza.toppings[topping] != null
+                    onEditPizza(pizza.withTopping(
+                        topping = topping,
+                        placement = if (isOnPizza) {
+                            null
+                        } else {
+                            ToppingPlacement.All
+                        }
+                    ))
+                }
+            )
+        }
+    }
+}
+
+@Composable
+private fun OrderButton(
+    pizza: Pizza,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        modifier = modifier,
+        onClick = { /*TODO*/ }
+    ) {
+        val currencyFormatter = remember {
+            NumberFormat.getCurrencyInstance()
+        }
+        val price = currencyFormatter.format(pizza.price)
+        Text(
+            text = stringResource(R.string.place_order_button, price)
+                .toUpperCase(Locale.current)
+        )
+    }
+}
